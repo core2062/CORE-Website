285862a9935fcb65ab36aa3633f0244014b76e26
Nick <zamorskin@msoe.edu>
2011-02-05 09:29:30 -0600

    Sidebar text update

--- sidebar.php
+++ sidebar.php
@@ -48,7 +48,7 @@
 
     <p><b>Team Location:</b> Waukesha, Wisconsin </p>
 
-    <p>Our name stands for Community of Robotic Engineers, and that is just what we are-we are from Waukesha, Wisconsin and accept students from any three of our community high schools. We are part of a larger organization called <span>FIRST</span>. To read more about them, read the section below. We pair them up with great mentors who teach them the fundamentals of robotics and other life skills. </p>
+    <p>Our name stands for Community of Robotic Engineers, and that is just what we are-we are from Waukesha, Wisconsin and accept students from any three of our community high schools, two preperatory academies, and homeschool students from the area. We are part of a larger organization called <span>FIRST</span>. To read more about them, read the section below. We pair them up with great mentors who teach them the fundamentals of robotics and other life skills. </p>
 
     <a href="<?php bloginfo('url'); ?>/about/" class="button">Read More</a> 
 
