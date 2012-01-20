00ef15cf1c105e83ff3022244cdea33d302f2032
Evan <eg1290@gmail.com>
2012-01-19 20:15:27 -0600

    Licensed website content under CC 3.0 Attribution

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="clear"></div>
 </div><!-- End page -->
-		<p class="footer">C.O.R.E. 2062 &copy; 2011 is proudly part of <a href="http://usfirst.org/" target="_blank">FIRST Robotics.</a> <span> 
-	<a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> </span> <!-- | <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></span>--></p>
+		<p class="footer">C.O.R.E. 2062 is proudly part of <a href="http://usfirst.org/" target="_blank">FIRST Robotics.</a> <span> 
+	<a style="margin-left:90px;" href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> </span> <span>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a></span> <!-- | <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></span>--></p>
 
 <?php wp_footer(); ?>
 	</div><!-- End page-wrap -->
