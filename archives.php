c7570c038af6b9569cd663e46dc189f24f79911c
Evan <eg1290@gmail.com>
2012-02-01 06:57:20 -0600

    Working on archive page

--- archives.php
+++ archives.php
@@ -11,15 +11,33 @@ Template Name: Archives
     </div><!-- End banner-bar -->
     <?php get_sidebar(); ?>
 <div class="main-content">
+	<div class="databoxpage archivebox">
+		<div class="titleblock archivetitle"><h3>Posts from This Year</h3></div>
+		<div class="archivepage-spacer"></div>
+		<?php query_posts( 'cat=3');?>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="post" id="post-<?php the_ID(); ?>">
-			<div class="databoxpage">
-				<div class="titleblock"><h3>Posts By Tag</div>
+			<div class="databoxpagepost">
+				<div class="titleblock"><h3 class="postpagetitle"><?php the_title(); ?></h3>
+					<span class="bylinepage">By <?php the_author() ?> on <?php the_time('F jS, Y') ?></span>
+				</div>
 				<div class="text">
-					<?php wp_list_categories(); ?>
+					<?php the_content(); ?>
 				</div><!-- End text -->
 			</div><!-- End databopage -->
-		</div><!--end post-->
-
+		</div>
+		<?php endwhile; else: ?> 
+		<p><?php _e(''); ?></p>
+		<?php endif; ?>
+	</div> <!--End databoxpage -->
+	<div class="archivepage-spacer"></div>
+	<div class="databoxpage">
+		<div class="text"></div>
+		<div class="titleblock"><h3>Posts from Previous Years</h3></div>
+		<div class="text">
+			<?php wp_list_categories('exclude=3,7'); ?>
+		</div><!-- End text -->
+	</div> <!--End second databoxpage -->
 </div><!-- End main-content -->
 
 
