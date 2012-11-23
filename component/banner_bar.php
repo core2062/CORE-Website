d488d34b0c236343d74c42607b16915850d2b633
Sean Lang <slang800@gmail.com>
2012-11-23 04:12:38 -0600

    small change

--- component/banner_bar.php
+++ component/banner_bar.php
@@ -2,6 +2,6 @@
 	<?php if ( function_exists('yoast_breadcrumb') ) {
 		yoast_breadcrumb('<p id="breadcrumbs">','</p>');
 	} ?>
-	<a href="/" title="Click here to return home" class="banner-title">Community of Robotic Engineers - Robotics Team 2062 - Waukesha, Wisconsin </a>
+	<a href="<?php bloginfo('url'); ?>" title="Click here to return home" class="banner-title">Community of Robotic Engineers - Robotics Team 2062 - Waukesha, Wisconsin </a>
 	<a class="learnmore" title="Click here to learn more!" href="/about">Learn More!</a>
 </div>
