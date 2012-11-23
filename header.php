e7b806919351efe27adf396288364ceb81f4e6c2
Sean Lang <slang800@gmail.com>
2012-11-23 03:31:04 -0600

    cu3er script should only be included on front page

--- header.php
+++ header.php
@@ -25,17 +25,20 @@
 	<?php }?>
 
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
-	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/swfobject/swfobject.js"></script>
-	<script type="text/javascript">
-		var flashvars = {};
-		flashvars.xml = "<?php bloginfo('template_url'); ?>/config.xml";
-		flashvars.font = "";
-		var params = {};
-		params.wmode = "transparent";
-		var attributes = {};
-		attributes.id = "slider";
-		swfobject.embedSWF("<?php bloginfo('template_url'); ?>/cu3er.swf", "cu3er-container", "900", "275", "9", "expressInstall.swf", flashvars,params, attributes);
-	</script>
+
+	<?php if(is_front_page()):?>
+		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/swfobject/swfobject.js"></script>
+		<script type="text/javascript">
+			var flashvars = {};
+			flashvars.xml = "<?php bloginfo('template_url'); ?>/config.xml";
+			flashvars.font = "";
+			var params = {};
+			params.wmode = "transparent";
+			var attributes = {};
+			attributes.id = "slider";
+			swfobject.embedSWF("<?php bloginfo('template_url'); ?>/cu3er.swf", "cu3er-container", "900", "275", "9", "expressInstall.swf", flashvars,params, attributes);
+		</script>
+	<?php endif; ?>
 
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
