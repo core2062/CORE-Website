a39ce3fe2104ddfb8ed6d352f6fb1603016d9476
Sean Lang <slang800@gmail.com>
2012-11-23 23:38:22 -0600

    fixing pages

--- page-custom.php
+++ page-custom.php
@@ -1,2 +1,21 @@
-<?php get_header(); ?>
-<?php get_footer(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+	<?php get_header(); ?>
+	<body>
+		<div class="page-wrap">
+
+			<?php include('component/header.php'); ?>
+
+			<div class="page">
+				<?php include('component/banner_bar.php'); ?>
+
+				<?php //get_sidebar(); ?>	
+				<div class="main-content"></div>
+				<div class="clear"></div>
+			</div>
+
+			<?php get_footer(); ?>
+
+		</div>
+	</body>
+</html>
--- page.php
+++ page.php
@@ -1,24 +1,42 @@
-<?php get_header(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+	<?php get_header(); ?>
+	<body>
+		<div class="page-wrap">
 
-<?php include('component/banner_bar.php'); ?>
+			<?php include('component/header.php'); ?>
 
-<?php get_sidebar(); ?>
+			<div class="page">
+				<?php include('component/banner_bar.php'); ?>
 
-<div class="main-content">
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<div class="post" id="post-<?php the_ID(); ?>">
-		<div class="databoxpage">
-			<div class="titleblock">
-				<h3><?php the_title(); ?></h3>
+				<?php get_sidebar(); ?>	
+
+				<div class="main-content">
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<div class="post" id="post-<?php the_ID(); ?>">
+						<div class="databoxpage">
+							<div class="titleblock">
+								<h3><?php the_title(); ?></h3>
+							</div>
+							<div class="text">
+								<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+								<?php wp_link_pages(
+									array(
+										'before' => '<p><strong>Pages:</strong> ',
+										'after' => '</p>',
+										'next_or_number' => 'number'
+									)
+								); ?>
+							</div>
+						</div>
+					</div>
+					<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+				</div>
+				<div class="clear"></div>
 			</div>
-			<div class="text">
-				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
-				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-			</div>
+
+			<?php get_footer(); ?>
+
 		</div>
-	</div>
-	<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
-</div>
-
-
-<?php get_footer(); ?>
+	</body>
+</html>
