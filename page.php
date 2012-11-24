715448459401acad11013128a3920e8771f567a5
Sean Lang <slang800@gmail.com>
2012-11-24 17:03:14 -0600

    fixing syntax error

--- page.php
+++ page.php
@@ -3,16 +3,12 @@
 	<?php get_header(); ?>
 	<body>
 		<div class="page-wrap">
-
 			<?php include('component/header.php'); ?>
-
 			<div class="page">
 				<?php include('component/banner_bar.php'); ?>
-
 				<?php get_sidebar(); ?>	
-
 				<div class="main-content">
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 					<div class="post" id="post-<?php the_ID(); ?>">
 						<div class="databoxpage">
 							<div class="titleblock">
@@ -20,23 +16,16 @@
 							</div>
 							<div class="text">
 								<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
-								<?php wp_link_pages(
-									array(
-										'before' => '<p><strong>Pages:</strong> ',
-										'after' => '</p>',
-										'next_or_number' => 'number'
-									)
-								); ?>
+								<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
 							</div>
 						</div>
 					</div>
+					<?php endwhile; endif; ?>
 					<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
 				</div>
 				<div class="clear"></div>
 			</div>
-
 			<?php get_footer(); ?>
-
 		</div>
 	</body>
 </html>
