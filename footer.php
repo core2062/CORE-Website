b0316cc3a6316d5a18ef6f83926ac045b8f982f4
Sean Lang <slang800@gmail.com>
2012-11-25 01:14:08 -0600

    issues with banner/navbar are fixed. badges still broken

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
-<img src="<?php bloginfo('template_url'); ?>/images/FRC-ExcelAward11.jpg" alt="Website Excellence Award" class="badge"/>
+<div id="badges">
+	<img src="<?php bloginfo('template_url'); ?>/images/FRC-ExcelAward11.jpg" alt="Website Excellence Award"/>
+</div>
 
 <div class="clear"></div>
 <p class="footer">
-	C.O.R.E. 2062 is proudly part of <a href="http://usfirst.org/" target="_blank">FIRST Robotics.</a>
+	C.O.R.E. 2062 is proudly part of <a href="http://usfirst.org/" target="_blank">FIRST Robotics</a>.
 	This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>
 	<a style="float:right" href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
 </p>
