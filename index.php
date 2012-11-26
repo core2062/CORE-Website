88da66821d06125621aa0bcd946f096577fe04bd
Sean Lang <slang800@gmail.com>
2012-11-25 19:03:28 -0600

    layout is nearly fixed... gotta add a reset for the font-size and need to fix margins that are overlapping

--- index.php
+++ index.php
@@ -16,38 +16,33 @@
 						<h4>Welcome!</h4>
 						<p>Welcome to the website for C.O.R.E. 2062! C.O.R.E. is a FIRST robotics team located in Waukesha, Wisconsin. If you want to find more information about FIRST, click on the logo to your left. This website is where you will find all of the information about our team. It is recommended that you check back here periodically, as we update often. You can search for a specific topic using the search bar above, or use the drop-down menus to look for something.</p>
 					</div>
-					<div class="postbox">
-						<div class="titleblock" title="Recent News">
-							<h3>Recent News</h3>
-						</div>
-						<ul>
-							<?php
-								global $more;
-								query_posts('posts_per_page=3');
-								if( have_posts() ) : while ( have_posts() ) : the_post(); $more = 1;
-							?>
-								
-								<li>
-									<div class="posttitle">
-										<a href="<?php the_permalink() ?>" rel="bookmark" title="Click here to read more"><?php the_title(); ?></a>
-										<span class="byline">By <?php the_author() ?> on <?php the_time('F jS, Y') ?></span>
-									</div>
-									<div class="posts">
-										<div class="post-content">
-											<?php the_content(); ?>
-										</div>
-									</div>
-								</li>
 
-							<?php endwhile; else: ?>
-
-								<p>
-									<?php _e('Sorry, no posts matched your criteria.'); ?>
-								</p>
-
-							<?php endif; ?>
-						</ul>
+					<div class="titleblock" title="Recent News">
+						<h3>Recent News</h3>
 					</div>
+					<?php
+						global $more;
+						query_posts('posts_per_page=3');
+						if( have_posts() ) : while ( have_posts() ) : the_post(); $more = 1;
+					?>
+						
+						<div class="box">
+							<div class="post-title">
+								<a href="<?php the_permalink() ?>" rel="bookmark" title="Click here to read more"><?php the_title(); ?></a>
+								<span class="byline">By <?php the_author() ?> on <?php the_time('F jS, Y') ?></span>
+							</div>
+							<div class="post-content">
+								<?php the_content(); ?>
+							</div>
+						</div>
+
+					<?php endwhile; else: ?>
+
+						<p>
+							<?php _e('Sorry, no posts matched your criteria.'); ?>
+						</p>
+
+					<?php endif; ?>
 					<a href="archives" class="archivelink">more posts</a>
 				</div>
 				<?php get_footer(); ?>
