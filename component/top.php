5c929acd6be4df4ba262a300e0b6560a83305c71
Sean Lang <slang800@gmail.com>
2012-12-24 00:32:34 -0600

    nav bar is looking & working much better

--- component/top.php
+++ component/top.php
@@ -115,11 +115,7 @@ class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
 	<?php if(is_front_page()):?>
 		<div class="header-show">
 			<div id="cu3er-container">
-				<br />
-				<br />
 				<p>Hey there...it looks like you don't have a current version of flash installed on your computer..or you have it disabled. Sorry about the inconvenience. If you'd like to see the magical thing that should be where I am, please enable or install flash using the link below. Thanks! If you don't care, then just pretend I'm not here.</p>
-				<br />
-				<br />
 				<a href="http://www.adobe.com/go/getflashplayer">
 					<img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" />
 				</a>
