f3bf2efb039aeb92bfbd1536fd3255376ce0e3e1
Sean Lang <slang800@gmail.com>
2012-12-25 15:26:16 -0600

    gutted out cu3er... replacing w/ better 3d css based slider... removes ~140kb

--- component/scripts.php
+++ component/scripts.php
@@ -1,15 +1,5 @@
 <?php if(is_front_page()):?>
-	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/swfobject/swfobject.js"></script>
-	<script type="text/javascript">
-		var flashvars = {};
-		flashvars.xml = "<?php bloginfo('template_url'); ?>/config.xml";
-		flashvars.font = "";
-		var params = {};
-		params.wmode = "transparent";
-		var attributes = {};
-		attributes.id = "slider";
-		swfobject.embedSWF("<?php bloginfo('template_url'); ?>/cu3er.swf", "cu3er-container", "100%", "275", "9", "expressInstall.swf", flashvars,params, attributes);
-	</script>
+
 <?php endif; ?>
 
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/js.js"></script>
--- component/top.php
+++ component/top.php
@@ -114,12 +114,7 @@ class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
 	<?php include (TEMPLATEPATH . '/searchform.php'); ?>
 	<?php if(is_front_page()):?>
 		<div class="header-show">
-			<div id="cu3er-container">
-				<p>Hey there...it looks like you don't have a current version of flash installed on your computer..or you have it disabled. Sorry about the inconvenience. If you'd like to see the magical thing that should be where I am, please enable or install flash using the link below. Thanks! If you don't care, then just pretend I'm not here.</p>
-				<a href="http://www.adobe.com/go/getflashplayer">
-					<img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" />
-				</a>
-			</div>
+
 		</div>
 	<?php else: ?>
 		<div class="header-short-pic"></div>
