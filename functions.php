1d40288cdd8262486adc11fc1fe810f7b4a02e3a
Sean Lang <slang800@gmail.com>
2013-01-20 13:16:43 -0800

    added html5 shim for ie versions less than 9 & updated jquery

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php
-if ( function_exists('register_sidebar') ) {
+// add ie conditional html5 shim to header
+function add_ie_html5_shim () {
+	global $is_IE;
+	if($is_IE)
+		echo '<!--[if lt IE 9]>';
+  		echo '<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>';
+		echo '<![endif]-->';
+}
+add_action('wp_head', 'add_ie_html5_shim');
+  
+if(function_exists('register_sidebar')){
 	register_sidebar(array(
 		'before_widget' => '<li id="%1$s" class="widget %2$s">',
 		'after_widget' => '</li>',
@@ -7,6 +17,7 @@ if ( function_exists('register_sidebar') ) {
 		'after_title' => '</h2>',
 	));
 }
+
 function register_my_menus() {
 	register_nav_menus(
 		array(
@@ -14,13 +25,15 @@ function register_my_menus() {
 		)
 	);
 }
-add_action( 'init', 'register_my_menus' );
+add_action('init', 'register_my_menus');
 
 //jQuery Insert From Google
-if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
 function my_jquery_enqueue() {
 	wp_deregister_script('jquery');
-	wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", false, null);
+	wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js", false, null);
 	wp_enqueue_script('jquery');
+}
+if (!is_admin()){
+    add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
 }
 ?>
