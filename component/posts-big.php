66215e44b061fad380930cace0978edc1036b7ae
Sean Lang <slang800@gmail.com>
2013-02-03 00:05:28 -0600

    added posts big for posts that can display full title n' stuff

---
+++ component/posts-big.php
@@ -0,0 +1,24 @@
+<?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<div class="titleblock" title="Recent News">
+		<h3>
+			<a href="<?php the_permalink() ?>" rel="bookmark" title="Click here to read more"><?php the_title(); ?></a>
+			<?php if(is_single($post)): ?>
+				<span class="byline">By <?php the_author() ?> on <?php the_time('F jS, Y') ?></span>
+			<?php endif; ?>
+		</h3>
+	</div>
+	<div class="break"></div>
+	<div class="box">
+		<div class="post-content">
+			<?php the_content(); ?>
+		</div>
+	</div>
+	<div class="break"></div>
+
+<?php endwhile; else: ?>
+
+	<p>
+		<?php _e('Sorry, no posts matched your criteria.'); ?>
+	</p>
+
+<?php endif; ?>
