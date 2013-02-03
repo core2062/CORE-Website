63ee9d2d3271834f97c2b6329337cc56997dfaea
Sean Lang <slang800@gmail.com>
2013-02-03 00:06:22 -0600

    simplified

--- page.php
+++ page.php
@@ -1,31 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-	<?php get_header(); ?>
-	<body>
-		<div id="page-wrap">
-			<?php include('component/top.php'); ?>
-			<div id="page">
-				<?php include('component/banner_bar.php'); ?>
-				<?php get_sidebar(); ?>	
-				<div class="main-content">
-					<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-					<div class="post" id="post-<?php the_ID(); ?>">
-						<div class="databoxpage">
-							<div class="titleblock">
-								<h3><?php the_title(); ?></h3>
-							</div>
-							<div class="text">
-								<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
-								<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-							</div>
-						</div>
-					</div>
-					<?php endwhile; endif; ?>
-					<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
-				</div>
-			</div>
-			<?php get_footer(); ?>
-		</div>
-	</body>
-	<?php include('component/scripts.php'); ?>
-</html>
+<?php ob_start(); ?>
+<div class="main-content no-sidebar">
+	<?php include('component/posts-big.php'); ?>
+</div>
+<?php
+	include("wrapper.php");
+?>
--- single.php
+++ single.php
@@ -1,53 +1,8 @@
+<?php ob_start(); ?>
+<div class="main-content no-sidebar">
+	<?php include('component/posts-big.php'); ?>
+</div>
+<?php
+	include("wrapper.php");
+?>
 
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-	<?php get_header(); ?>
-	<body>
-		<div id="page-wrap">
-			<?php include('component/top.php'); ?>
-			<div id="page">
-				<?php include('component/banner_bar.php'); ?>
-				<?php get_sidebar(); ?>	
-
-				<div class="main-content">
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<div class="post" id="post-<?php the_ID(); ?>">
-						<div class="databoxpagepost">
-							<div class="titleblock">
-								<h3 class="postpagetitle"><?php the_title(); ?></h3>
-								<span class="bylinepage">By <?php the_author() ?> on <?php the_time('F jS, Y') ?></span>
-							</div>
-							<div class="text">
-								<?php the_content(); ?>
-							</div>
-						</div>
-
-						<?php endwhile; else: ?> 
-							<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-						<?php endif; ?>
-					
-						<p>
-							<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-								// Both Comments and Pings are open ?>
-								You can <a href="#respond">leave a response</a>, or <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> from your own site.
-							<?php } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-								// Only Pings are Open ?>
-								Responses are currently closed, but you can <a href="<?php trackback_url(); ?> " rel="trackback">trackback</a> from your own site.
-							<?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-								// Comments are open, Pings are not ?>
-								You can skip to the end and leave a response. Pinging is currently not allowed.
-							<?php } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-								// Neither Comments, nor Pings are open ?>
-								Both comments and pings are currently closed.
-							<?php } edit_post_link('Edit this entry','','.'); ?>
-						</p>
-					</div>
-					<?php comments_template(); ?>
-				</div>
-			</div>
-			<?php get_footer(); ?>
-		</div>
-	</body>
-	<?php include('component/scripts.php'); ?>
-</html>
