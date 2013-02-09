5edec3f6ef79ebde3330ba2b6f73af324ef87972
Featherdragon15 <featherdragon15@gmail.com>
2013-02-09 11:36:33 -0600

    Fixed the searchbar to work in all browsers. Removed the orange expanse of nothingness

--- searchform.php
+++ searchform.php
@@ -1,6 +1,5 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-	<input type="text" onblur="if (this.value == '') {this.value = 'Find info with keywords..';}" onfocus="if (this.value == 'Find info with keywords..') {this.value = '';}" value="<?php the_search_query(); ?>" name="s" id="s" alt="Search Form" />
-	<input type="submit" id="searchsubmit" alt="Search Submit" value="" />
+	<input type="text" onblur="if (this.value == '') {this.value = 'Find info with keywords..';}" onfocus="if (this.value == 'Find info with keywords..') {this.value = '';}" value="<?php the_search_query(); ?>" name="s" id="s" alt="Search Form" /><input type="submit" id="searchsubmit" alt="Search Submit" value="" />
 </form>
 
 
