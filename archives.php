337bb2e3d3537f06501cf5314b10ce6d3c63f355
agilgenbach <al@gilgenbach.net>
2014-02-13 14:50:11 -0600

    modified:   archives.php	Updated category references to point to this years posts.

--- archives.php
+++ archives.php
@@ -4,7 +4,7 @@
 		<h3>Posts from This Year</h3>
 	</div>
 	<div class="break"></div>
-	<?php query_posts('cat=' . get_category_by_slug('12-13')->term_id);?>
+	<?php query_posts('cat=' . get_category_by_slug('2013-2014')->term_id);?>
 	<?php include('component/posts.php'); ?>
 
 	<div class="titleblock">
@@ -12,7 +12,7 @@
 	</div>
 	<div class="box">
 		<div class="post-content">
-			<?php wp_list_categories("exclude=" . get_category_by_slug('12-13')->term_id); ?>
+			<?php wp_list_categories("exclude=" . get_category_by_slug('2013-2014')->term_id); ?>
 		</div>
 	</div>
 </div>
