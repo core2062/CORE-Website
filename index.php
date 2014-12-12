757cca3f988bef02a26d1cb63ed11629f58b2033
agilgenbach <al@gilgenbach.net>
2014-12-11 18:38:54 -0600

    Added light box for team overview video.

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php ob_start(); ?>
 <div class="main-content">
 	<div class="alert blue">
-		<h4>Welcome!</h4>
+		<h4>Welcome! <a class="fancybox-youtube" style="color: #0066B3" href="https://www.youtube.com/watch?v=SNQBKtsNRQM"><strong>Check us out in action here!</strong></a></h4>
 		<p>
-			We are a group of high school students and adult volunteers that learn, explore, and celebrate science and technology. Through the international organization FIRST, For Inspiration and Recognition in Science and Technology, we build a robot to compete each year. We experience excitement of the challenge, create life-long relationships, and have fun. But we do much more than that. Through collaboration and leadership from students and mentors, ours students develop work ethic, professionalism, and pride.
+			We are a group of high school students and adult volunteers that learn, explore, 
+			and celebrate science and technology. Through the international organization FIRST, For 
+			Inspiration and Recognition in Science and Technology, we build a robot to compete each year. 
+			We experience excitement of the challenge, create life-long relationships, and have fun. 
+			But we do much more than that. Through collaboration and leadership from students and mentors, ours 
+			students develop work ethic, professionalism, and pride.
 		</p>
 		<p>
-			Our team encourages younger students to experience the same excitement of engineering through programs like FLL and VEX. The Waukesha VEX League Competition and League Nights are hosted by our team. Along with VEX, we also hold FLL competitions and encourage members to mentor FLL teams. In both of these venues, and in many others, we display our robot and enlighten them about FRC. Through this, we excite, inspire and celebrate engineering.
+			Our team encourages younger students to experience the same excitement of engineering 
+			through programs like FLL and VEX. The Waukesha VEX League Competition and League Nights 
+			are hosted by our team. Along with VEX, we also hold FLL competitions and encourage members to mentor FLL teams. In both of these venues, and in many others, we display our robot and enlighten them about FRC. Through this, we excite, inspire and celebrate engineering.
 		</p>
 	</div>
 	<div class="break"></div>
